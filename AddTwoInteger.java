25981a889e01bb38a83ebc8f5d843a81e61ac7f4
AiswaryasNair <160515100+AiswaryasNair@users.noreply.github.com>
2024-02-22 19:27:27 +0530

    committing add two integer program

---
+++ AddTwoInteger.java
@@ -0,0 +1,10 @@
+//write a program to add two integer numbers
+
+class AddTwoInteger{
+	public static void main(String args[]){
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+	int c= a+b;
+	System.out.println("Sum is:"+c);
+}
+}
