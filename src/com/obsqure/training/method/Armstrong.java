a8fb1cb745f5574efedf0d2d7673c9f5d406b3d8
ammua <ammua@AMMU21515>
2024-02-25 22:26:05 +0530

    committing updated code

--- src/com/obsqure/training/method/Armstrong.java
+++ src/com/obsqure/training/method/Armstrong.java
@@ -1,23 +1,22 @@
 package com.obsqure.training.method;
 
 public class Armstrong {
-
 	public static void main(String[] args) {
-        int number = Integer.parseInt(args[0]); 
-        int originalNumber = number;
-        int result = 0;
-        int power = String.valueOf(number).length();
+		int number = Integer.parseInt(args[0]);
+		int originalNumber = number;
+		int result = 0;
+		int power = String.valueOf(number).length();
 
-        while (number != 0) {
-            int digit = number % 10;
-            result += Math.pow(digit, power);
-            number /= 10;
-        }
+		while (number != 0) {
+			int digit = number % 10;
+			result += Math.pow(digit, power);
+			number /= 10;
+		}
 
-        if (result == originalNumber) {
-            System.out.println(originalNumber + " is an Armstrong number.");
-        } else {
-            System.out.println(originalNumber + " is not an Armstrong number.");
-        }
-    }
+		if (result == originalNumber) {
+			System.out.println(originalNumber + " is an Armstrong number.");
+		} else {
+			System.out.println(originalNumber + " is not an Armstrong number.");
+		}
+	}
 }
